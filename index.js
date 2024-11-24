1dfb409cd306895326c06b8efcb1a161a2e58b95
manin-nick <manin.nick@gmail.com>
2024-11-24 20:13:35 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// let a = 10 + 5
+// let b = 10 - 5
+// let c = 10 * 5
+// let d = 10 / 5
+// let e = 10 % 5
+// let f = 10 ** 5
+// let g = 15
+// g++
+// let i = 15
+// i--
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(i)
+
+
+// let isEqual = 5
+// console.log(isEqual == '5')
+// console.log(isEqual === '5')
+// console.log(isEqual != '5')
+// console.log(isEqual !== '5')
+
+// let a = 5
+// let b = 10
+// let c = 5
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a >= c)
+// console.log(a <= c)
+// console.log(a < c)
+
+// let a = true
+// let b = false
+// console.log(a || b) //или
+// console.log(a && b) //и
+// console.log(!a) //противоположное
+// console.log(!b)
+
+
+// let c = false || false && false // 0 + 0 * 0 = 0 (false)
+let c = true || false && false // 1 + 0 * 0 = 1 (true)
+console.log(c)
